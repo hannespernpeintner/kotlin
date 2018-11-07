e2866edf9eacb6805cb8ffa5c77f48b5048f6526
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2018-11-07 06:14:24 +0300

    Actualize Deprecated annotation and DeprecationLevel enum docs
    ...to clear some confusion from KT-25643.

--- core/builtins/src/kotlin/Annotations.kt
+++ core/builtins/src/kotlin/Annotations.kt
@@ -21,17 +21,20 @@ import kotlin.annotation.AnnotationRetention.SOURCE
 import kotlin.annotation.AnnotationTarget.*
 
 /**
- * Marks the annotated class, function, property, variable or parameter as deprecated.
- * @property message the message explaining the deprecation and recommending an alternative API to use.
- * @property replaceWith if present, specifies a code fragment which should be used as a replacement for
- *     the deprecated API usage.
+ * Marks the annotated class, typealias, function, property or property accessor as deprecated.
+ *
+ * @property message The message explaining the deprecation and recommending an alternative API to use.
+ * @property replaceWith If present, specifies a code fragment which should be used as a replacement for
+ *  the deprecated API usage.
+ * @property level Specifies how the deprecated element usages are reported in code.
+ *  See the [DeprecationLevel] enum for the possible values.
  */
 @Target(CLASS, FUNCTION, PROPERTY, ANNOTATION_CLASS, CONSTRUCTOR, PROPERTY_SETTER, PROPERTY_GETTER, TYPEALIAS)
 @MustBeDocumented
 public annotation class Deprecated(
-        val message: String,
-        val replaceWith: ReplaceWith = ReplaceWith(""),
-        val level: DeprecationLevel = DeprecationLevel.WARNING
+    val message: String,
+    val replaceWith: ReplaceWith = ReplaceWith(""),
+    val level: DeprecationLevel = DeprecationLevel.WARNING
 )
 
 /**
@@ -53,12 +56,14 @@ public annotation class Deprecated(
 public annotation class ReplaceWith(val expression: String, vararg val imports: String)
 
 /**
- * Contains levels for deprecation levels.
+ * Possible levels of a deprecation. The level specifies how the deprecated element usages are reported in code.
+ *
+ * @see Deprecated
  */
 public enum class DeprecationLevel {
-    /** Usage of the deprecated element will be marked as a warning. */
+    /** Usage of the deprecated element will be reported as a warning. */
     WARNING,
-    /** Usage of the deprecated element will be marked as an error. */
+    /** Usage of the deprecated element will be reported as an error. */
     ERROR,
     /** Deprecated element will not be accessible from code. */
     HIDDEN
